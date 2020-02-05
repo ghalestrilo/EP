e7dc774cdd05e775b912eab4bdbecffa1c14f9a8
ghales.trilo@gmail.com <ghales.trilo@gmail.com>
2020-02-05 19:24:47 -0300

    :truck: moving stuff around to allow modularization

--- code/1-vos-los-sabes.py
+++ code/1-vos-los-sabes.py
@@ -11,6 +11,9 @@
     # Hacer mas prolijas y emocionantes las transiciones
     # No abusar del platillo invertido, usarlo en los momentos claves
 
+### IMPORTS ###
+from eplib.functions import countRepetitions, reproducir
+
 Root.default = -3
 Scale.default = 'minor'
 Clock.bpm=100
@@ -117,18 +120,6 @@ def reset():
     pass
 
 ### REPRODUCTOR ###
-
-def countRepetitions(fname,cancion):
-    fnames = list(map(lambda x : x.__name__,cancion))
-    return fnames.count(fname)
-def reproducir(cancion,efectos,reset,start):
-    reset()
-    if len(cancion) > 0:
-        cancion[0]()
-        fname = cancion[0].__name__
-        if fname in efectos and countRepetitions(fname,cancion) == efectos[fname]["vuelta"]:
-            efectos[fname]["efecto"]()
-        Clock.schedule(lambda : reproducir(cancion[1:],efectos,reset,start+8),start + 8)
 
 start = Clock.mod(8) - 0.1
 Clock.schedule(lambda : reproducir(cancion,efectos,reset,start), start)
---
+++ code/eplib/functions.py
@@ -0,0 +1,11 @@
+def countRepetitions(fname,cancion):
+    fnames = list(map(lambda x : x.__name__,cancion))
+    return fnames.count(fname)
+def reproducir(cancion,efectos,reset,start):
+    reset()
+    if len(cancion) > 0:
+        cancion[0]()
+        fname = cancion[0].__name__
+        if fname in efectos and countRepetitions(fname,cancion) == efectos[fname]["vuelta"]:
+            efectos[fname]["efecto"]()
+        Clock.schedule(lambda : reproducir(cancion[1:],efectos,reset,start+8),start + 8)
